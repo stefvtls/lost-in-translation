1ecb83bab98e4427e2953ec6c71b952dde3d1f3a
stefvtls <stefvtls@gmail.com>
2023-02-22 18:31:08 +0100

    BUGFIX: BUGFIX3: display only 10 latest translations, not all the translations from API

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,8 @@
 
 
 const ProfileContainer = (props) => {
-    const listOfTranslations = props.user.translations.map( (t,index)=>
+    //STEFVTLS props.user.translations.slice(0,10).reverse().map().... creating a copy of array from api and reversing it to display only latest 10 items
+    const listOfTranslations = props.user.translations.slice(0).reverse().slice(0,10).map( (t,index)=>
         <div className="alert alert-warning" role="alert" key={index}>
             {t}
         </div>
@@ -10,7 +11,9 @@ const ProfileContainer = (props) => {
     return (
     <div className="css-container">
         <div className={'container'}>
-            <h1>Welcome, {props.user.username}</h1>
+            <h1>Welcome back, {props.user.username}</h1>
+            <h5> Here is the list of your 10 last translations in order from the most recent to the latest </h5>
+            <br></br>
             <ul> {listOfTranslations} </ul>
         </div>
     </div>
