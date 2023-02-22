39372089c1754cd1b7f0513f5d4ae7fef3f5a6bf
sahilofficial97 <91385151+sahilofficial97@users.noreply.github.com>
2023-02-22 11:16:34 +0100

    adding translation api call

---
+++ src/api/translationsAPI.js
@@ -0,0 +1,41 @@
+import { createHeaders } from "."
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const updateTranslations = async(user, translation) => {
+    try {
+        const response = await fetch(`${apiUrl}/${user.id}`, {
+            method:"PATCH",
+            headers: createHeaders(),
+            body: JSON.stringify({
+                translations: [...user.translations,translation ]
+            })
+        })
+        if(!response.ok){
+            throw new Error("Could not update the translations.")
+        }
+        const result = await response.json()
+        return [null, result]
+    } catch (error) {
+        return [error.message, null]
+    }
+}
+
+export const clearTranslations = async(user) => {
+    try {
+        const response = await fetch(`${apiUrl}/${user.id}`, {
+            method:"PATCH",
+            headers: createHeaders(),
+            body: JSON.stringify({
+                translations: []
+            })
+        })
+        if(!response.ok){
+            throw new Error("Could not update the translations.")
+        }
+        const result = await response.json()
+        return [null, result]
+    } catch (error) {
+        return [error.message, null]
+    }
+}
--- src/views/TranslationPage.jsx
+++ src/views/TranslationPage.jsx
@@ -2,13 +2,18 @@ import TranslateContainer from "../components/Translate/TranslateContainer"
 import './TranslationPage.css'
 
 const TranslationPage = () => {
+
+    const handleInput=(event)=>{
+        console.log(event.target.value)
+    }
+
     return (
         <div className={'yellow-box'}>
             <div className={'container'}>
                 <br/>
                 <div className={'row'}>
                     <div className="col-md-9">
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Hello"/>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Hello" onChange={handleInput}/>
                     </div>
                     <div className="col-md-2">
                         <button className={'btn btn-primary'}>Go</button>
