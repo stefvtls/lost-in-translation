8e22b8bcacbf939c48e025d039be6b128a6f5fdc
sahilofficial97 <91385151+sahilofficial97@users.noreply.github.com>
2023-02-23 14:53:54 +0100

    remove whitespace and symbol from translation and prevent the same value to be save again.

--- src/views/TranslationPage.jsx
+++ src/views/TranslationPage.jsx
@@ -12,20 +12,25 @@ const TranslationPage = () => {
 
     const [requestedTranslation, setRequestedTranslation] = useState([])
 
+    const [lastTranslation,setlastTranslation] = useState([''])
+
 
     //this function will split the word into pieces and send it sends a http request
     const handleTranslateClicked =async({ translationRequest }) => {
-        const newTranslation = translationRequest.split("")
+        //this will remove white space in translation
+        let newTrans=translationRequest.replace( /\s/g, '')
+        //this will remove symbols in translation
+        let removeSymbol=newTrans.replace(/[^a-zA-Z ]/g, "")
+        let newTranslation = removeSymbol.split("")
         setRequestedTranslation(newTranslation)
         // STEFVTLS: if no input do not bother for translations, just ignore it,
         // we will not update the API and current state, so empty translation will not show up on the profile page
-        if (newTranslation.length !== 0 ) {
-
+        if (newTranslation.length !== 0 && removeSymbol!==lastTranslation) {
             const [error,result] = await updateTranslations(currentUser, translationRequest)
 
             // STEFVTLS: you need to change the state of the user except of changing the API, otherwise API updates, but the components in the app does not know about it
             setCurrentUser(result)
-
+            setlastTranslation(removeSymbol)
             console.log('Error', error)
             console.log('Result', result)
         } 
